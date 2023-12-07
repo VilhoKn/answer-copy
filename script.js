e9bf07ea1a28818bf75930d3c805420ac57defd9
unknown <knuutila.vilho@gmail.com>
2023-12-08 01:11:28 +0200

    made more stuff

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const copyButton = document.querySelector('.copy')
+const copyButton = document.querySelector('#copy')
 
 copyButton.addEventListener('click', () => {
 	console.log("copy")
+	copyTest()
 })
+
+async function copyTest() {
+	const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+  	const response = await chrome.tabs.sendMessage(tab.id, {greeting: "hello"});
+  	// do something with response here, not outside the function
+	console.log(response);
+}
