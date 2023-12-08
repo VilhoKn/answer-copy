c750468c8e1adb46339de2ad750c28fca21b3360
Vilho Kn <73425056+VilhoKn@users.noreply.github.com>
2023-12-08 09:15:11 +0000

    added db stuff

--- content.js
+++ content.js
@@ -22,20 +22,69 @@ function initSanomapro() {
 		buttonContainer.appendChild(button);
 	}, 1500);
 
-}
+}  
 
 function saveSanomapro() {
 	const questionTypeMap = {
 		"OpenQuestionModelAnswerInteraction": 0,
-		"fill-in": 1,
-		"drag-and-drop": 2,
-		"matching": 3,
-		"open-ended": 4,
-		"multiple-answer": 5
+		"ClozeCombiInteraction": 1,
+		"ChoiceInteractionXopus": 2,
 	};
+
+	const questionType = questionTypeMap[document.querySelector("app-document").getAttribute("content-type")]
+	const questionPath = getCurrentTab().url.split("content-feed/")[1]
+	const answers = getSanomaAnswers()
+
+	const urlEndpoint = "https://eu-central-1.aws.data.mongodb-api.com/app/data-mgjos/endpoint/data/v1"
+
+	const options = {
+		url: urlEndpoint+"/insertDocument",
+		method: 'POST',
+		headers: {
+		  'Accept': 'application/json',
+		  'Content-Type': 'application/json',
+		  'apiKey': ""
+		},
+		data: JSON.stringify({
+			
+			"dataSource": "mongodb-atlas",
+			"database": "Cluster0",
+			"collection": "sanomapro",
+			"document": {
+				name: getName(),
+				questionType,
+				questionPath,
+				answers,
+			}
+			
+		})
+	};
+
+}
+
+function getSanomaAnswers(type) {
+	if(type === 0) {
+
+	}
+	else if(type === 1) {
+
+	}
+	else if(type === 2) {
+
+	}
+}
+
+function getName() {
+	return "Vilho"
 }
 
 
 function initOtava() {
 	console.log("Otava");
 }
+
+function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    let [tab] = chrome.tabs.query(queryOptions);
+    return tab;
+  }
